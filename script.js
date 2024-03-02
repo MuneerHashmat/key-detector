1c093bcffdbdfe652323ded7cf042c43ab45a68c
Muneer Hashmat <muneerhashmat@gmail.com>
2024-03-02 16:42:02 +0530

    removed sound

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 let keyPressed = document.getElementById("key");
 let keyCode = document.getElementById("keycode");
-let sound = document.getElementById("sound");
 
 window.addEventListener("keydown", (e) => {
-    sound.play();
     keyPressed.innerHTML = `The key pressed is <span>${e.key}</span>`;
     keyCode.innerHTML = `<span>${e.keyCode}</span>`;
 });
