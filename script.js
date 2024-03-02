30eda118b422b74e9e482e9f6f3b2b14bb2cb99a
Muneer Hashmat <muneerhashmat@gmail.com>
2024-03-02 16:39:44 +0530

    added sound on keypress

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let keyPressed = document.getElementById("key");
 let keyCode = document.getElementById("keycode");
+let sound = document.getElementById("sound");
 
 window.addEventListener("keydown", (e) => {
+    sound.play();
     keyPressed.innerHTML = `The key pressed is <span>${e.key}</span>`;
-    keyCode.innerHTML = `<span>${e.keyCode}</span>`
-    console.log(e);
+    keyCode.innerHTML = `<span>${e.keyCode}</span>`;
 });
